6fc216bc44b85cb65d72e101343efcff383eea7d
Geoo Chi <geoochi@geoochi.com>
2025-02-28 16:35:31 +0800

    update

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -5,18 +5,20 @@ const Home: React.FC = () => {
   const { user, isAuthenticated } = useAuthStore()
   return (
     <div className='space-y-6 text-center'>
-      <h1 className='text-3xl font-bold'>Welcome to Revite</h1>
-      <hr />
       {isAuthenticated ? (
         <p>Hello, {user?.name || user?.email} !</p>
       ) : (
-        <p className='text-gray-500 dark:text-gray-400'>
-          Please{' '}
-          <Link to='/signin' className='underline'>
-            sign in
-          </Link>{' '}
-          to continue.
-        </p>
+        <>
+          <h1 className='text-3xl font-bold'>Welcome to Revite</h1>
+          <hr />
+          <p className='text-gray-500 dark:text-gray-400'>
+            Please{' '}
+            <Link to='/signin' className='underline'>
+              sign in
+            </Link>{' '}
+            to continue.
+          </p>
+        </>
       )}
     </div>
   )
