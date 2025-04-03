28816b4b89311c1483173cd7cab4d4cbae00cb6c
Geoo Chi <geoochi@geoochi.com>
2025-04-03 20:20:48 +0800

    update

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -51,7 +51,7 @@ const Navbar: React.FC = () => {
           ) : (
             <>
               <Button variant='outline'>
-                <Link to='/signup'>Sign up</Link>
+                <Link to='/signin'>Sign In</Link>
               </Button>
             </>
           )}
