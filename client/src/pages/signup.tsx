7ae53232533a5d30b96340feba33bd63bd008dc9
Geoo Chi <geoochi@geoochi.com>
2025-04-03 20:25:37 +0800

    update

--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -9,7 +9,7 @@ import useAuthStore from '@/lib/store'
 import { useEffect } from 'react'
 
 const registerSchema = z.object({
-  name: z.string().min(2).optional(),
+  name: z.string().min(2),
   email: z.string().email(),
   password: z.string().min(6),
 })
