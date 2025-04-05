d5942fc3b999cf6dbec3aeba21a90d3473a7658a
Geoo Chi <geoochi@geoochi.com>
2025-04-05 18:37:10 +0800

    update

--- server/src/revite-server.ts
+++ server/src/revite-server.ts
@@ -7,8 +7,6 @@ app.use(express.json())
 
 app.use('/api/auth', authRoutes)
 
-const PORT = process.env.PORT
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`)
 })
